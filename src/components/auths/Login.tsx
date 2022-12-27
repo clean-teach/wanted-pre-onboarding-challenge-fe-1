0e72e5e770ab2827a503abcf15977805ce421d0f
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-12-28 04:10:14 +0900

    레파지토리 생성 및 초기 환경 설정 등
    # 설치 패키지 및 개발 환경
    
    - `create-react-app`
    - `typescript`
    - `styled-components`
    - `react-router-dom`
    - `recoil`
    - `react-hook-form`
    - `axios`
    - `react-query`
    
    # API 테스트 등

---
+++ src/components/auths/Login.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from "axios";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+
+function Login() {
+      // const fetcher = async () => {
+  //   return await axios.post('http://localhost:8080/users/create', {
+  //       email: '11',
+  //       password: '11',
+  //     });
+  // }
+  // const { isLoading, data } = useQuery('queryKey', fetcher);
+  // useEffect( () => {
+  //   isLoading ? console.log('loading') : console.log(data);
+  // }, [isLoading, data]);
+    return (
+        <h1>
+            login
+        </h1>
+    );
+}
+
+export default Login;
---
+++ src/router/Router.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Login from "../components/auths/Login";
+import SignUp from "../components/auths/SignUp";
+import TodoList from "../components/todos/TodoList";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/auth',
+                children: [
+                    {
+                        path: 'login',
+                        element: <Login />
+                    },
+                    {
+                        path: 'signup',
+                        element: <SignUp />
+                    },
+                ]
+            },
+            {
+                path: 'todos',
+                element: <TodoList />
+            },
+        ],
+    },
+]);
+
+export default router;
