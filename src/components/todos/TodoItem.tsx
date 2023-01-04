699d3f39d38dd12e5bd87b3142e4fe348522848c
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-01-05 01:45:27 +0900

    스타일 추가
    현재 선택 된 To do 표시

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { fetchDeleteTodos } from '../../api';
@@ -8,9 +9,15 @@ const Wrapper = styled.li`
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 4rem;
-  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
   padding: 1rem;
   border-radius: 0.25rem;
+  &.isClicked {
+    font-weight: bold;
+    transform: scale(1.04);
+    box-shadow: 0 0 2rem rgba(0, 0, 0, 0.4);
+    transition: 0.5s;
+  }
   a:hover {
     text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
     transition: 0.2s;
@@ -27,6 +34,7 @@ interface IProps {
 }
 
 function TodoItem({ token, todoId, todoTitle, todoCurrent }: IProps) {
+  const params = useParams();
   const setTodos = useSetRecoilState(TodosState);
   const onRemove = () => {
     const response = fetchDeleteTodos({ todoId, token });
@@ -37,8 +45,12 @@ function TodoItem({ token, todoId, todoTitle, todoCurrent }: IProps) {
       .catch((error) => console.log(error));
   };
 
+  useEffect(() => {
+    console.log(params);
+  }, [params]);
+
   return (
-    <Wrapper>
+    <Wrapper className={todoId === params.todoId ? 'isClicked' : ''}>
       <Link to={`${todoId}`}>{todoTitle}</Link>
       <DeleteBtn onClick={onRemove}>삭제</DeleteBtn>
     </Wrapper>
